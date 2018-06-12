663dfedd27a4f1496194b325a0abc85122b17800
Arik <arik.yudin@aperio-systems.com>
2018-06-12 22:29:49 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public title = 'app';
+  public frameSrc: SafeUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit() {
+    this.frameSrc = this.sanitizer.bypassSecurityTrustResourceUrl(location.hash.slice(1));
+  }
 }
